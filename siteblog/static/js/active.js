827f9067ee5624bb8fc21cba5b6fc98e003e1b4c
Temirlan Baysynov <tima.baysynov.2017@gmail.com>
2021-04-18 14:58:30 +0600

    Active menu

--- siteblog/static/js/active.js
+++ siteblog/static/js/active.js
@@ -2,9 +2,12 @@ $(document).ready(function () {
 
     $('.menu a').each(function(){
         let location = window.location.protocol + '//' + window.location.host +
-         window.location.pathname;
+   window.location.pathname;
+        let link = this.href;
+        if(location == link){
+            $(this).parent().addClass('active');
+        }
     });
-
 
     var filterFns = {
         numberGreaterThan50: function () {
